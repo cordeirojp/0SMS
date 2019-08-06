b16ae075fb03a0735262bbd449382c741b4f8af0
cordeirojp <jean-pierre.cordeiro@ca-ts.fr>
2019-08-06 18:22:53 +0200

    final

--- 0SMS/lambda.js
+++ 0SMS/lambda.js
@@ -3,12 +3,9 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
-
     let receiver = event['receiver'];
     let sender = event['sender'];
     let message = event['message'];
-
-
 
     console.log("Sending message", message, "to receiver", receiver);
 
@@ -28,23 +25,18 @@ exports.handler = function (event, context, callback) {
     }).promise()
         .then(data => {
             console.log("Sent message to", receiver);
-            console.log("data =", data);
-            //console.log("data =",data.ResponseMetadata.MessageId);
             var msgID = data.MessageId;
-            console.log("msgID =", msgID);
 
             ddb.put({
                 TableName: 'SendedSMS',
                 Item: { 'MessID': msgID, 'Receiver': receiver, 'Message': message }
             }).promise()
                 .then((data) => {
-                    //your logic goes here
-                    console.log("db ok", data);
+                    console.log("db ok");
                 })
                 .catch((err) => {
                     console.log("db failed", err);
                 });
-
 
             callback(null, data);
         })
