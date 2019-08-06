53b95ae23997bca1e21f886a29d2c892a3ef7e95
cordeirojp <jean-pierre.cordeiro@ca-ts.fr>
2019-08-06 17:39:19 +0200

    add db

--- 0SMS/lambda.js
+++ 0SMS/lambda.js
@@ -1,13 +1,15 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 const sns = new AWS.SNS();
+
 exports.handler = function (event, context, callback) {
 
     let receiver = event['receiver'];
     let sender = event['sender'];
     let message = event['message'];
 
-   
-   
+
+
     console.log("Sending message", message, "to receiver", receiver);
 
     sns.publish({
@@ -25,11 +27,22 @@ exports.handler = function (event, context, callback) {
         PhoneNumber: receiver
     }).promise()
         .then(data => {
-			console.log("Sent message to", receiver);
-			callback(null, data);
+            console.log("Sent message to", receiver);
+            ddb.put({
+                TableName: 'SendedSMS',
+                Item: { 'MessID': data }
+            }).promise()
+                .then((data) => {
+                    //your logic goes here
+                })
+                .catch((err) => {
+                    //handle error
+                });
+
+            callback(null, data);
         })
         .catch(err => {
-			console.log("Sending failed", err);
-			callback(err);
+            console.log("Sending failed", err);
+            callback(err);
         });
 }
